f791d1a833c206a0ab71ede318dbcf753a0bea5d
Arseniy Savin <zhf2003@yandex.ru>
2022-10-10 22:51:14 +0600

    Fixed typos in build.yml and send_report.sh

--- .github/send_report.sh
+++ .github/send_report.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
-docs=$(ls ./coveragereport )
+docs=$(ls ./coveragereport | grep .htm)
 
+# Delete previous report
+curl -X "DELETE" -H "Token: ${TOKEN}" ${REPORT_URI}/${BRANCH_NAME}
+
+# Send new report files
 for doc in $docs
 do
     echo "Sending $doc..."
-    curl -H "Token: ${TOKEN}" -H "Content-Type: text/html" -X POST --data "@./coveragereport/$doc" ${REPORT_URI}/${BRANCH_NAME}/$doc
+    curl -H "Token: ${TOKEN}" -H "Content-Type: text/html" -H "X-File-Name: ${doc}" -X POST --data "@./coveragereport/$doc" ${REPORT_URI}/${BRANCH_NAME}
     rm "./coveragereport/$doc"
 done
