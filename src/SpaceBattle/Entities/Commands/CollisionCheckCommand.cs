2947a37b14bc45e954e2f68c3d51508d7dc4cdcc
Arseniy Savin <zhf2003@yandex.ru>
2022-12-19 13:31:30 +0000

    Added descriptions to lib classes

--- src/SpaceBattle/Entities/Commands/CollisionCheckCommand.cs
+++ src/SpaceBattle/Entities/Commands/CollisionCheckCommand.cs
@@ -3,6 +3,27 @@ namespace SpaceBattle.Entities.Commands;
 using SpaceBattle.Base;
 using SpaceBattle.Collections;
 
+// Description:
+//      Implementation of a command that checks two objects for a collision.
+//      When running the Run() method, checks if there was a collision between the first and second object.
+// Parametres:
+//      Constructor takes next parametres:
+//          IUObject first:
+//              The first object to check for collision with the second.
+//          IUObject second:
+//              The second object to check for collision with the first.
+// Container dependencies:
+//      bool Events.Collision.Determinant (IUObject, IUObject):
+//          Strategy to detect if there is a collision between two IUObjects.
+//
+//          Returns:
+//              true:
+//                  A collision has occured.
+//              false:
+//                  There was no collision between these two objects.
+// Exceptions:
+//      Exception:
+//          A collision has occured.
 public class CollisionCheckCommand : ICommand
 {
     private IUObject first, second;
--- src/SpaceBattle/Entities/Commands/StartMoveCommand.cs
+++ src/SpaceBattle/Entities/Commands/StartMoveCommand.cs
@@ -2,6 +2,44 @@ namespace SpaceBattle.Entities.Commands;
 using SpaceBattle.Base;
 using SpaceBattle.Collections;
 
+// Description:
+//      Initializes a long running IUObject motion command.
+// Parametres:
+//      Constructor takes next parametres:
+//          IMoveCommandStartable mcs:
+//              The object that is the target to start moving.
+// Container dependencies:
+//      ICommand Object.SetupProperty (IUObject, string key, object value):
+//          Command that sets the IUObject value with the given key.
+//      
+//          Returns:
+//              Command that sets the value when the Run() method is called.
+//      
+//      IMovable Entities.Adapter.IMovable(IMoveCommandStartable):
+//          An adapter that casts an IMoveCommandStartable to an IMovable interface.
+//      
+//          Returns:
+//              Returns an IMovable-adapted object.
+//
+//      ICommand Entities.Commands.MoveCommand(IMovable):
+//          Command that performs the movement of IMovable object.
+//          
+//          Returns:
+//              A command that perform the movement of the object on
+//              Run() method call.
+//      
+//      ICommand Collections.Queue.Push (IQueue, ICommand):
+//          The command that pushes the command (argv[1]) to the given queue (argv[0]).
+//      
+//      Parametres:
+//          IQueue<ICommand> argv[0]:
+//              The queue to which the command will be added.
+//          ICommand argv[1]:
+//              The command to be added to the queue.
+//
+//      Returns:
+//          ICommand:
+//              The command that, when the Run() method is called, will add the command(argv[1]) to the queue(argv[0]).
 public class StartMoveCommand : ICommand
 {
     private IMoveCommandStartable mcs;
