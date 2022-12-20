4e4643292f043a6cf30e155694d1fc385941dede
Arseniy Savin <zhf2003@yandex.ru>
2022-12-20 15:15:00 +0600

    Implemented exception handler find strategy

---
+++ src/SpaceBattle/Entities/Strategies/ExceptionHandlerFindStrategy.cs
@@ -0,0 +1,19 @@
+namespace SpaceBattle.Entities.Strategies;
+
+using SpaceBattle.Base;
+using SpaceBattle.Collections;
+
+public class ExceptionHandlerFindStrategy : IStrategy
+{
+    public object Run(params object[] argv)
+    {
+        ICommand command = (ICommand)argv[0];
+        Exception exception = (Exception)argv[1];
+
+        var container = new Container();
+
+        var ExceptionHandlers = container.Resolve<IDictionary<ICommand, IDictionary<Exception, IStrategy>>>("Handler.Exception");
+
+        return ExceptionHandlers[command][exception];
+    }
+}
---
+++ src/SpaceBattleTests/Entities/Strategies/ExceptionHandlerFindStrategy_tests.cs
@@ -0,0 +1,48 @@
+namespace SpaceBattleTests.Entities.Strategies;
+
+using SpaceBattle.Base;
+using SpaceBattle.Entities.Strategies;
+using SpaceBattle.Collections;
+
+using Moq;
+
+public class ExceptionHandlerFindStrategyTests {
+
+    [Fact(Timeout = 1000)]
+    void ExceptionHandlerFindStrategy_Successful() {
+        // Init dependencies
+        var container = new Container();
+        container.Resolve<ICommand>(
+            "Scopes.Current.Set", 
+            container.Resolve<object>(
+                "Scopes.New", container.Resolve<object>("Scopes.Root")
+            )
+        ).Run();
+        container.Resolve<ICommand>("IoC.Register","Handler.Exception", typeof(DictionaryInjectionStrategy)).Run();
+
+        var ehfs = new ExceptionHandlerFindStrategy();
+
+        // Action
+        Assert.NotNull(ehfs.Run(
+            new Mock<ICommand>().Object,
+            new Mock<Exception>().Object
+            ));
+    }
+}
+
+class DictionaryInjectionStrategy : IStrategy
+{
+    public object Run(params object[] argv)
+    {
+        var Handler = new Mock<IStrategy>();
+
+        var ExceptionDict = new Mock<IDictionary<Exception, IStrategy>>();
+        ExceptionDict.Setup(ed => ed[It.IsAny<Exception>()]).Returns(Handler.Object);
+
+        var MagicDict = new Mock<IDictionary<ICommand, IDictionary<Exception, IStrategy>>>();
+
+        MagicDict.Setup(md => md[It.IsAny<ICommand>()]).Returns(ExceptionDict.Object);
+
+        return MagicDict.Object;
+    }
+}
