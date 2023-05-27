93a67372ffa95e891e707a8d0988dac4096b1bb9
Arseniy Savin <ayusavin@fantast.dev>
2023-05-27 07:19:32 +0600

    Fixed quantum, added pullable read cmd

---
+++ src/SpaceBattle/Entities/Commands/PullableBehaviourCommand.cs
@@ -0,0 +1,20 @@
+namespace SpaceBattle.Entities.Commands;
+
+using SpaceBattle.Base;
+
+public class PullableBehaviourCommand : ICommand
+{
+    private IPullable<ICommand> src;
+    private IStrategy behaviour;
+
+    public PullableBehaviourCommand(IPullable<ICommand> source, IStrategy behaviour)
+    {
+        this.src = source;
+        this.behaviour = behaviour;
+    }
+
+    public void Run()
+    {
+        this.behaviour.Run(this.src);
+    }
+}
---
+++ src/SpaceBattleTests/Entities/Commands/PullableBehaviourCommand_tests.cs
@@ -0,0 +1,33 @@
+namespace SpaceBattleTests.Entities.Commands;
+
+using Moq;
+using SpaceBattle.Base;
+using SpaceBattle.Collections;
+using SpaceBattle.Entities.Commands;
+
+
+
+public class PullableBehaviourCommandTests
+{
+    [Fact(Timeout = 1000)]
+    void ExecutesBehaviourAgainstPullable_Successful()
+    {
+        // Init dependencies
+        var behaviour = new Mock<IStrategy>();
+        var pullable = new Mock<IPullable<ICommand>>();
+
+        behaviour.Setup(b => b.Run(It.IsAny<object[]>())).Callback(
+            (object[] argv) =>
+            {
+                var Pullable = (IPullable<ICommand>)argv[0];
+
+                Assert.Same(Pullable, pullable.Object);
+            }
+        );
+
+        var pbc = new PullableBehaviourCommand(pullable.Object, behaviour.Object);
+
+        // Assertation
+        pbc.Run();
+    }
+}
